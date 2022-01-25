c892fc66372f73cf7a13316af2f7ba9058a789f8
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-25 21:40:12 +0800

    Fixed model unique prop

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    username: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     img: { type: String },
     role: { type: String, default: "user", enum: ["user", "admin"] },
