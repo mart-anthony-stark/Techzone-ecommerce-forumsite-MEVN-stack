e8c83f8e1e5a3fd60fed3ff27860caac57cb625d
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 20:02:18 +0800

    added password minlength

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, minlength: 8 },
     img: { type: String },
     role: { type: String, default: "user", enum: ["user", "admin"] },
   },
