feb8bc932bb7a47f8f293940bb00d70077491b40
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 20:02:42 +0800

    added signup route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,5 +3,6 @@ const router = new Router({ prefix: "/auth" });
 const authController = require("../controllers/auth.controller");
 
 router.post("/login", authController.login);
+router.post("/signup", authController.signup);
 
 module.exports = router;
