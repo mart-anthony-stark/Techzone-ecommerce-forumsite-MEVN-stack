6b730d9ecc38b0d53564fe458c9b48a5a76d152d
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 20:02:07 +0800

    added signup method

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
+const { createToken } = require("../utils/token");
 
 const login = async (ctx) => {
   const { username, email, password } = ctx.request.body;
@@ -7,24 +8,46 @@ const login = async (ctx) => {
     const user = await User.findOne({ $or: [{ username }, { email }] });
     if (!user) {
       ctx.status = 404;
-      ctx.body = { msg: "Account not found" };
+      ctx.body = { success: false, msg: "Account not found" };
+      return;
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       ctx.status = 401;
-      ctx.body = { msg: "Incorrect password" };
+      ctx.body = { success: false, msg: "Incorrect password" };
+      return;
     }
+    const token = createToken(user);
+    user._doc.password = undefined;
+    ctx.status = 200;
+    ctx.body = { success: true, user: user._doc, token };
   } catch (error) {
+    console.log(error);
     ctx.status = 500;
     ctx.body = { msg: error };
   }
 };
 
-const signup = (ctx) => {
+const signup = async (ctx) => {
   const { name, username, email, password } = ctx.request.body;
   try {
+    const user = new User({
+      name,
+      username,
+      email,
+      password: password,
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    const token = createToken(user);
+    user._doc.password = undefined;
+    ctx.status = 200;
+    ctx.body = { success: true, user: user._doc, token };
   } catch (error) {
+    console.log(error);
     ctx.status = 500;
     ctx.body = { msg: error };
   }
