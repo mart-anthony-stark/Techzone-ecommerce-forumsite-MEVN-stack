bf1095f1aba3b7f659cfdca5efe998c6dbaa9ce2
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 19:41:52 +0800

    added mongoose connection

--- api/app.js
+++ api/app.js
@@ -1,14 +1,27 @@
 const Koa = require("koa");
 const router = require("./routes");
+const koaBody = require("koa-body");
+const mongoose = require("mongoose");
+require("dotenv").config({});
 
 const port = process.env.PORT || 5000;
 
 const app = new Koa();
 
 // middlewares
+app.use(koaBody({ multipart: true }));
 app.use(router());
 
-app.listen(port, (e) => {
+app.listen(port, async (e) => {
   if (e) return console.log(e);
   console.log(`Server started running at port ${port}`);
+
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+    });
+    console.log("Connected to database");
+  } catch (e) {
+    console.log(e);
+  }
 });
