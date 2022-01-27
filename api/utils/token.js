5fcc650aeea1cda460cca1479bce200d65e9db2e
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-27 19:03:12 +0800

    added verify token

--- api/utils/token.js
+++ api/utils/token.js
@@ -4,4 +4,29 @@ const createToken = (user) => {
   return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
 
+const verifyToken = (ctx, next) => {
+  const authHeader = ctx.request.headers.token;
+  if (!authHeader) {
+    ctx.status = 403;
+    ctx.body = { error: "Unauthenticated" };
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      ctx.status = 403;
+      ctx.body = { error: "Invalid token" };
+      return;
+    }
+
+    req.user = user;
+    await next();
+  });
+};
+
+const verifyTokenAndAuth = (ctx, next) => {
+  
+}
+
 module.exports = { createToken };
