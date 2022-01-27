dc24ff8454a30d5da93ad9c8bbf38c9f40dcb839
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-27 20:06:13 +0800

    added update product route

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -7,5 +7,6 @@ const { verifyTokenAndAdmin, verifyToken } = require("../utils/token");
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.post("/", verifyTokenAndAdmin, productController.createProduct);
+router.put("/:id", verifyTokenAndAdmin, productController.updateProduct);
 
 module.exports = router;
