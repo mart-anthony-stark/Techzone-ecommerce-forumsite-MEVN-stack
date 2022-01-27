026ca6d7cf991404ea4bb56cb4ddb6e4630aa37a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-27 19:30:47 +0800

    added product router

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,8 @@
 const combineRouters = require("koa-combine-routers");
 const rootRouter = require("./root.route");
 const authRouter = require("./auth.route");
+const productRouter = require("./product.route");
 
-const router = combineRouters(rootRouter, authRouter);
+const router = combineRouters([rootRouter, authRouter, productRouter]);
 
 module.exports = router;
