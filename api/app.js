b82a10200627c3eae7c05fb09ce479e402fcd23c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 19:54:51 +0800

    registered product route

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 // routes
 fastify.register(require("./routes/auth.route"));
+fastify.register(require("./routes/product.route"));
 
 fastify.listen(port, async (e) => {
   if (e) return console.log(e);
