43d9138dfa581d5a2681324b575e22125975919a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 20:30:43 +0800

    added get post by id controller

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -9,4 +9,22 @@ module.exports = {
       reply.code(500).send({ error });
     }
   },
+
+  getPostById: async (req, reply) => {
+    try {
+      const post = await Post.find({ _id: req.params.id });
+      reply.code(200).send(post);
+    } catch (error) {
+      reply.code(500).send({ error });
+    }
+  },
+
+  createPost: async (req, reply) => {
+    try {
+      const post = await Post.find({ _id: req.params.id });
+      reply.code(200).send(post);
+    } catch (error) {
+      reply.code(500).send({ error });
+    }
+  },
 };
