2714997376763514b490c12e852d4b90a12ff075
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 20:58:57 +0800

    added populate to post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,8 +3,11 @@ const Post = require("../models/Post.model");
 module.exports = {
   getAllPosts: async (req, reply) => {
     try {
-      const posts = await Post.find();
-      reply.send(posts);
+      const posts = await Post.find().populate("author");
+      posts.forEach((post) => {
+        post.author.password = undefined;
+      });
+      reply.status(200).send(posts);
     } catch (error) {
       reply.code(500).send({ error });
     }
@@ -21,9 +24,21 @@ module.exports = {
 
   createPost: async (req, reply) => {
     try {
-      const post = await Post.find({ _id: req.params.id });
+      const { title, body } = req.body;
+      const post = await new Post({ author: req.user._id, title, body }).save();
+      reply.code(200).send("posted");
+    } catch (error) {
+      reply.code(500).send({ error });
+    }
+  },
+
+  deletePost: async (req, reply) => {
+    try {
+      console.log(req.params.id);
+      const post = await Post.findOneAndRemove({ _id: req.params.id });
       reply.code(200).send(post);
     } catch (error) {
+      console.log(error);
       reply.code(500).send({ error });
     }
   },
