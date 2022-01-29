22c60e1e710fded1ca8970f19ea6a8bb74a7aeee
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 21:24:08 +0800

    fixed setup

--- api/app.js
+++ api/app.js
@@ -1,9 +1,8 @@
-const fastify = require("fastify")({ logger: true });
+const fastify = require("fastify")();
 const mongoose = require("mongoose");
 require("dotenv").config({});
 
 const port = process.env.PORT || 5000;
-
 // routes
 fastify.register(require("./routes/auth.route"));
 fastify.register(require("./routes/product.route"));
@@ -11,13 +10,13 @@ fastify.register(require("./routes/post.route"));
 
 fastify.listen(port, async (e) => {
   if (e) return console.log(e);
-  fastify.log.info(`Server started running at port ${port}`);
+  console.log(`Server started running at port ${port}`);
 
   try {
     await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
     });
-    fastify.log.info("Connected to database");
+    console.log("Connected to database");
   } catch (e) {
     fastify.log.error(e);
     process.exit(1);
