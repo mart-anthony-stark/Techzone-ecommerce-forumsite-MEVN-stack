aa89d2bcdead08219174fe5b6e9bcde865ad8fd4
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 19:55:34 +0800

    fixed prehandler functions

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,13 +1,25 @@
 const productController = require("../controllers/product.controller");
 
-const { verifyTokenAndAdmin, verifyToken } = require("../utils/token");
+const { verifyAdmin, verifyToken } = require("../utils/token");
 
 const productRoutes = (fastify, options, done) => {
-  fastify.get("/", productController.getAllProducts);
-  fastify.get("/:id", productController.getProductById);
-  fastify.post("/", verifyTokenAndAdmin, productController.createProduct);
-  fastify.put("/:id", verifyTokenAndAdmin, productController.updateProduct);
-  fastify.delete("/:id", verifyTokenAndAdmin, productController.deleteProduct);
+  fastify.get("/products", productController.getAllProducts);
+  fastify.get("/products/:id", productController.getProductById);
+  fastify.post(
+    "/products",
+    { preHandler: [verifyToken, verifyAdmin] },
+    productController.createProduct
+  );
+  fastify.put(
+    "/products/:id",
+    { preHandler: [verifyToken, verifyAdmin] },
+    productController.updateProduct
+  );
+  fastify.delete(
+    "/products/:id",
+    { preHandler: [verifyToken, verifyAdmin] },
+    productController.deleteProduct
+  );
   done();
 };
 
