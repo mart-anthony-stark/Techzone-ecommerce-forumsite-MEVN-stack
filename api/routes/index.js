11075c6bcdbe65439bad88cf432ef1a4376d3e4e
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 19:26:25 +0800

    deleted index route

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const combineRouters = require("koa-combine-routers");
-const rootRouter = require("./root.route");
-const authRouter = require("./auth.route");
-const productRouter = require("./product.route");
-
-const router = combineRouters([rootRouter, authRouter, productRouter]);
-
-module.exports = router;
