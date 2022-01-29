7391cd6e6fcce4d8c231a6c8a22522883f605a1e
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 19:09:10 +0800

    migrated from koa to fastify

--- api/app.js
+++ api/app.js
@@ -1,27 +1,23 @@
-const Koa = require("koa");
-const router = require("./routes");
-const koaBody = require("koa-body");
+const fastify = require("fastify")({ logger: true });
+
 const mongoose = require("mongoose");
 require("dotenv").config({});
 
 const port = process.env.PORT || 5000;
 
-const app = new Koa();
-
 // middlewares
-app.use(koaBody({ multipart: true }));
-app.use(router());
 
-app.listen(port, async (e) => {
+fastify.listen(port, async (e) => {
   if (e) return console.log(e);
-  console.log(`Server started running at port ${port}`);
+  fastify.log.info(`Server started running at port ${port}`);
 
   try {
     await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
     });
-    console.log("Connected to database");
+    fastify.log.info("Connected to database");
   } catch (e) {
-    console.log(e);
+    fastify.log.error(e);
+    process.exit(1);
   }
 });
