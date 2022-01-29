84201787fafd8ac0daa75ce09cef79275b3e35e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 20:30:55 +0800

    added create post route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,7 +1,14 @@
 const postController = require("../controllers/post.controller");
 
+const { verifyToken } = require("../utils/token");
 const postRoutes = (fastify, options, done) => {
   fastify.get("/posts", postController.getAllPosts);
+  fastify.get("/posts/:id", postController.getPostById);
+  fastify.post(
+    "/posts",
+    { preHandler: [verifyToken] },
+    postController.createPost
+  );
   done();
 };
 
