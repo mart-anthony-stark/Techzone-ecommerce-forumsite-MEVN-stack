1d63a44ebd0d4b2c7eba2023c52492884d46b312
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-29 19:30:54 +0800

    migrated from koa to fastify

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,13 +1,14 @@
-const Router = require("@koa/router");
-const router = new Router({ prefix: "/products" });
 const productController = require("../controllers/product.controller");
 
 const { verifyTokenAndAdmin, verifyToken } = require("../utils/token");
 
-router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getProductById);
-router.post("/", verifyTokenAndAdmin, productController.createProduct);
-router.put("/:id", verifyTokenAndAdmin, productController.updateProduct);
-router.delete("/:id", verifyTokenAndAdmin, productController.deleteProduct);
+const productRoutes = (fastify, options, done) => {
+  fastify.get("/", productController.getAllProducts);
+  fastify.get("/:id", productController.getProductById);
+  fastify.post("/", verifyTokenAndAdmin, productController.createProduct);
+  fastify.put("/:id", verifyTokenAndAdmin, productController.updateProduct);
+  fastify.delete("/:id", verifyTokenAndAdmin, productController.deleteProduct);
+  done();
+};
 
-module.exports = router;
+module.exports = productRoutes;
