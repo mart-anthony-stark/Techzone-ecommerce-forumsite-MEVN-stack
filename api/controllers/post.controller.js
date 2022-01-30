999933988025d7e78a9b5b2624caa5d5efb1e79c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 19:31:41 +0800

    added update post method

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -38,10 +38,30 @@ module.exports = {
 
   deletePost: async (req, reply) => {
     try {
-      const post = await Post.findOneAndRemove({ _id: req.params.id });
+      const post = await Post.findOneAndRemove({
+        author: req.user._id,
+        _id: req.params.id,
+      });
       reply.code(200).send(post);
     } catch (error) {
       console.log(error);
+      reply.code(500).send({ error });
+    }
+  },
+
+  updatePost: async (req, reply) => {
+    if (req.body.author) req.body.author = undefined;
+    try {
+      const updatedPost = await Post.findOneAndUpdate(
+        {
+          author: req.user._id,
+          _id: req.params.id,
+        },
+        { $set: req.body },
+        { new: true }
+      );
+      reply.code(200).send(updatedPost);
+    } catch (error) {
       reply.code(500).send({ error });
     }
   },
