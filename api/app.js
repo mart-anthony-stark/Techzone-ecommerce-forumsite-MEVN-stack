b06ec79cd6d4fd4e74ede2ab2bbfd167f9912000
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 19:14:31 +0800

    fixed logger

--- api/app.js
+++ api/app.js
@@ -9,14 +9,14 @@ fastify.register(require("./routes/product.route"));
 fastify.register(require("./routes/post.route"));
 
 fastify.listen(port, async (e) => {
-  if (e) return console.log(e);
-  console.log(`Server started running at port ${port}`);
+  if (e) return fastify.log.error(e);
+  fastify.log.info(`Server started running at port ${port}`);
 
   try {
     await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
     });
-    console.log("Connected to database");
+    fastify.log.info("Connected to database");
   } catch (e) {
     fastify.log.error(e);
     process.exit(1);
