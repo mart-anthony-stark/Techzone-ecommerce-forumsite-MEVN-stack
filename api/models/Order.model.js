aa1c79385a6c2f2608b5f35a3803dab89520b160
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-31 20:27:41 +0800

    added order status enum

--- api/models/Order.model.js
+++ api/models/Order.model.js
@@ -17,7 +17,11 @@ const orderShema = new mongoose.Schema(
     ],
     amount: { type: Number, required: true },
     address: { type: Object, required: true },
-    status: { type: String, default: "pending" },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "shipped", "delivered"],
+    },
   },
   { timestamps: true }
 );
