a7ad597d53f5cd22ed26e9d94427126cd495946b
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-31 20:05:39 +0800

    added get specific user orders route

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,11 +1,16 @@
 const orderController = require("../controllers/order.controller");
 
-const { verifyToken, verifyAdmin } = require("../utils/token");
+const { verifyToken, verifyAdmin, verifyUID } = require("../utils/token");
 const orderRoutes = (fastify, options, done) => {
   fastify.get(
     "/orders",
     { preHandler: [verifyToken, verifyAdmin] },
     orderController.getAllOrders
+  );
+  fastify.get(
+    "/orders/find/:uid",
+    { preHandler: [verifyToken, verifyUID] },
+    orderController.getUserOrders
   );
   done();
 };
