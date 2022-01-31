d40e6c2c94aa9aecffe5977b3d9dd5c9d0c8f9cb
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-31 19:47:26 +0800

    added order schema

---
+++ api/models/Order.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const orderShema = new mongoose.Schema(
+  {
+    customer: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        productId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderShema);
