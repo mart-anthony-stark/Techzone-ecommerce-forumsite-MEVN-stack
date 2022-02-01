f50daea3145b38bff3236e651c8013074ed71ddb
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-01 19:57:23 +0800

    added get cart by user controller

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -1,7 +1,10 @@
-const Cart = require('../models/Cart.model')
+const Cart = require("../models/Cart.model");
 
 module.exports = {
-    getAllCarts: async (req,reply) => {
-        reply.send('carts')
-    }
-}
+  getAllCarts: async (req, reply) => {
+    reply.send("carts");
+  },
+  getCartByUser: async (req, reply) => {
+    reply.send(req.params.uid);
+  },
+};
