fe412997c830232d348fa7802e91a5c2bbbd6cd9
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-01 20:00:45 +0800

    added get carts controllers

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -2,9 +2,19 @@ const Cart = require("../models/Cart.model");
 
 module.exports = {
   getAllCarts: async (req, reply) => {
-    reply.send("carts");
+    try {
+      const carts = await Cart.find();
+      reply.code(200).send(carts);
+    } catch (error) {
+      reply.code(500).send({ error });
+    }
   },
   getCartByUser: async (req, reply) => {
-    reply.send(req.params.uid);
+    try {
+      const cart = await Cart.find({ userId: req.params.uid });
+      reply.code(200).send(cart);
+    } catch (error) {
+      reply.code(500).send({ error });
+    }
   },
 };
