08339f749f3d23a50c30031f0e953f5f0719f039
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-05 22:15:08 +0800

    added bearer token to fetch

--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -1,7 +1,9 @@
 export default async ({ store }) => {
   const token = localStorage.getItem('token')
   if (token) {
-    const res = await fetch(`${process.env.baseUrl}/auth/logged`)
+    const res = await fetch(`${process.env.baseUrl}/auth/logged`, {
+      headers: { 'Content-Type': 'application/json', token: `bearer ${token}` },
+    })
     const data = await res.json()
     console.log(data)
     if (res.status === 200) {
