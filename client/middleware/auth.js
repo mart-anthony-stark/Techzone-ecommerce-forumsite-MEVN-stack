4e6b7d16022ba9548b232fc5a5ad3c07385b53df
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-05 21:52:13 +0800

    added fetch if logged

--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -1,3 +1,10 @@
-export default function (context) {
-  // console.log(context)
+export default async ({ store }) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    const res = await fetch(`${process.env.baseUrl}/auth/logged`)
+    const data = await res.json()
+    console.log(data)
+  }else{
+    
+  }
 }
