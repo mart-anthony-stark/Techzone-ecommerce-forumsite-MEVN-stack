592f9b32b0914aadaec3d0b27795a85ab7a3e331
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-05 21:53:04 +0800

    added is logged authentication route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,10 @@
 const authController = require("../controllers/auth.controller");
 
+const { verifyToken } = require("../utils/token");
 const authRoutes = (fastify, options, done) => {
+  fastify.get("/auth/logged", { preHandler: [verifyToken] }, (req, reply) => {
+    reply.code(200).send({ message: "Authenticated" });
+  });
   fastify.post("/auth/login", authController.login);
   fastify.post("/auth/register", authController.signup);
   done();
