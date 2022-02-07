a45274241cdc0c39ecfd094c3debd396e976b44e
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-07 20:52:15 +0800

    add: pagination to posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,16 @@
 const postController = require("../controllers/post.controller");
+const PostModel = require("../models/Post.model");
 
 const { verifyToken } = require("../utils/token");
+const paginate = require("../utils/pagination");
+
 const postRoutes = (fastify, options, done) => {
   fastify.get("/posts", postController.getAllPosts);
+  fastify.get(
+    "/posts/paginated",
+    { preHandler: [paginate(PostModel)] },
+    postController.getPaginated
+  );
   fastify.get("/posts/:id", postController.getPostById);
   fastify.post(
     "/posts/create",
