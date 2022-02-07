e2e2733c5cc351e304abcd4c6be0396fb0852c75
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-07 20:39:48 +0800

    added pagination

--- api/utils/pagination.js
+++ api/utils/pagination.js
@@ -1,7 +1,34 @@
 const paginate = (model) => {
   return async (req, reply, next) => {
-    req.paginated = "asd";
-    next();
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    
+    try {
+      results.result = await model.find().limit(limit).skip(startIndex).exec();
+      req.results = results;
+      next();
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   };
 };
 
