78037804c77e0c3ce7bc93df6c5f4450ad5a208d
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-08 19:07:23 +0800

    fixed loading state

--- client/components/PostFetcher.vue
+++ client/components/PostFetcher.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="fetch-post p-8 w-full"></div>
+  <div id="fetcher" class="fetch-post p-8 w-full"></div>
 </template>
 
 <script>
 export default {
-  props: ['index'],
+  props: ['index', 'loading'],
   data() {
     return {
       limit: 5,
     }
   },
   mounted() {
-    console.log(this.index, this.limit)
+    const root = document.querySelector('#fetcher')
+    const options = {
+      //   root: document.querySelector('#fetcher'),
+      rootMargin: '0px',
+      threshold: 1.0,
+    }
+    const observer = new IntersectionObserver(() => {
+      if (!this.loading) {
+        console.log('Fetching...')
+      }
+    }, options)
+
+    observer.observe(root)
   },
 }
 </script>
